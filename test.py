a9f3019e31cc95b654fadd7f6b8617903e65b954
youssefmohamed2 <67026144+youssefmohamed2@users.noreply.github.com>
2021-06-17 04:55:34 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,5 @@
+#my name is youssef im 15 years old and im making a simple calculator
+number1 = input("Enter a number: ")
+number2 = input("Enter another number: ")
+result = float(number1)+float(number2)
+print(result)
