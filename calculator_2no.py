b0590b9d4a97944f12e78eb31d95f1ad74a97fe0
omarxmax2233 <89490321+omarxmax2233@users.noreply.github.com>
2021-10-05 23:23:09 +0200

    Add files via upload

---
+++ calculator_2no.py
@@ -0,0 +1,18 @@
+def calc():
+
+	x = float(input('enter first number'))
+	y = input('choose the sign')
+	h = float(input('enter first number'))
+
+	if y == '+' :
+		print(x + h)
+		print('you are donkey go to learn math')
+	if y == '-' :
+		print(x - h)
+		print('you are donkey go to learn math')
+	if y == '*' :
+		print(x * h)
+		print('you are donkey go to learn math')
+	if y == '/' :
+		print(x/ h)
+		print('you are donkey go to learn math')
